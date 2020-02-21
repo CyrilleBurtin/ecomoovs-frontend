d1d0c088a4025a9317777b1d03e592435706e337
Cyrille <cyrille.burtin@gmail.gmail.com>
2020-02-21 10:16:12 +0100

    overflowx patch

--- src/moovs/components/moovCard/MoovCards.js
+++ src/moovs/components/moovCard/MoovCards.js
@@ -11,21 +11,21 @@ const MoovsCards = props => {
           key={i + 1}
           className='MoovCards'          
         >
-          <div style={{height: '150px', overflow:'hidden'}}>
-            <img src={e.img.eager[0].secure_url} style={{width:'100%', maxHeight:'150px'}} alt="moov"/>
-          </div> 
-          <div className='CardBody'>
-            <div>
-              {i + 1}. {e.name}
+            <div style={{height: '150px', overflow:'hidden'}}>
+              <img src={e.img.eager[0].secure_url} style={{width:'100%', maxHeight:'150px'}} alt="moov"/>
+            </div> 
+            <div className='CardBody'>
+                <div>
+                  {i + 1}. {e.name}
+                </div>
+                <div>{e.description}</div>
+                <div>{e.location.city}</div>
+                <div></div>
+                <div>{e.type}</div>
             </div>
-            <div>{e.description}</div>
-            <div>{e.location.city}</div>
-            <div></div>
-            <div>{e.type}</div>
-          </div>
-          <div style={{textAlign:'center'}}>
-            <BlueButton>Voir +</BlueButton>
-          </div>
+            <div style={{textAlign:'center'}}>
+              <BlueButton>Voir +</BlueButton>
+            </div>
         </div>
       ))}
     </div>
