7dc39bad0add2954fe6a0b854977e68d7cf4102e
Cyrille <cyrille.burtin@gmail.gmail.com>
2020-02-21 10:54:06 +0100

    test

--- src/header/pages/Header.js
+++ src/header/pages/Header.js
@@ -23,7 +23,7 @@ const Header = props => {
 
       burger.addEventListener('click', () => {
         nav.classList.toggle('nav-active');
-        admin.classList.remove('nav-active');
+        admin.classList.remove('admin-active');
       });
 
       nav.addEventListener('mouseleave', () => {
@@ -31,12 +31,12 @@ const Header = props => {
       })
 
       initials.addEventListener('click', () => {
-        admin.classList.toggle('nav-active');
+        admin.classList.toggle('admin-active');
         nav.classList.remove('nav-active');
       });
 
       admin.addEventListener('mouseleave', () => {
-        admin.classList.remove('nav-active');
+        admin.classList.remove('admin-active');
       })
     };
 
